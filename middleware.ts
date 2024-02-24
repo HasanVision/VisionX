0308cecc86381e797e566d530a3dcbb5729257c9
HasanVision <hasan.visionx@gmail.com>
2024-02-23 20:49:01 -0500

    signin, siginout fixed

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,37 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 
+import {
+    apiAuthPrefix,
+    DEFAULT_LOGIN_REDIRECT,
+    publicRoutes,
+    authRoutes
+} from "@/routes";
+import {NextResponse} from "next/server";
+
 const {auth} = NextAuth(authConfig);
 export default auth((req) => {
+    const {nextUrl} = req;
     const isLoggedIn = !!req.auth;
-    console.log("IS Logged IN: " , isLoggedIn)
-    // req.auth
-    console.log("ROUTE:", req.nextUrl.pathname)
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+        return NextResponse.next();
+    }
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+        return NextResponse.next();
+    }
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL( "/auth/login", nextUrl))
+    }
+    return NextResponse.next();
+
 })
 
 // Optionally, don't invoke Middleware on some paths
---
+++ routes.ts
@@ -0,0 +1,38 @@
+/**
+ * An array of routes that are accessible without authentication
+ * @type{string[]}
+ */
+
+export const publicRoutes = [
+    "/"
+];
+
+/**
+ * an array of routes to be used for authentication
+ * these routes will direct after login to /settings.
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+
+];
+
+/**
+ * API authentication routes prefix
+ * this is used for api authentication and should be accessible for longed in and logged-out users
+ * @type{string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+
+/**
+ * Default redirect after log in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
+
+
+
+
+
