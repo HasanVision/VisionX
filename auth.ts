6308c2a28278b99656e206a3a8b1ca116c160726
HasanVision <hasan.visionx@gmail.com>
2024-03-17 19:54:07 -0400

    redirect to last login page works great!

--- auth.ts
+++ auth.ts
@@ -23,14 +23,14 @@ export const {
         signIn: "/auth/login",
         error: "/auth/error",
     },
-    // events: {
-    //     async linkAccount({ user }) {
-    //         await db.user.update({
-    //             where: { id: user.id },
-    //             data: { emailVerified: new Date() }
-    //         })
-    //     }
-    // },
+    events: {
+        async linkAccount({ user }) {
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date() }
+            })
+        }
+    },
     callbacks: {
         async signIn({ user, account }) {
             if (account?.provider !== "credentials") return true;
